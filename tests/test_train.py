44571ec9dcd6dbe94be86746fc9c86d47c58f98b
AnushkaUnhalkar <165803273+AnushkaUnhalkar@users.noreply.github.com>
2026-01-06 10:50:56 +0530

    Modify test_train.py to include project root path
    Added project root to Python path for test access.

--- tests/test_train.py
+++ tests/test_train.py
@@ -1,11 +1,17 @@
 import os
+import sys
 import joblib
+
+# Add project root to Python path
+ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.insert(0, ROOT_DIR)
+
 from src.train import train_model
 
+
 def test_model_training():
-    # Ensure the models directory is clean before the test runs
     train_model()
     assert os.path.exists("models/model.pkl")
 
     model = joblib.load("models/model.pkl")
-    assert hasattr(model, "predict") # Check if the model has a predict method 
+    assert hasattr(model, "predict")
