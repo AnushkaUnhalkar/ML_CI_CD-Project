6fee63b2e47c871455c89b13a10645c20147932f
Anushka Unhalkar <your_email@example.com>
2026-01-06 11:14:34 +0530

    first commit

--- tests/test_train.py
+++ tests/test_train.py
@@ -1,13 +1,6 @@
 import os
-import sys
 import joblib
-
-# Add project root to Python path
-ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-sys.path.insert(0, ROOT_DIR)
-
 from src.train import train_model
-
 
 def test_model_training():
     train_model()
